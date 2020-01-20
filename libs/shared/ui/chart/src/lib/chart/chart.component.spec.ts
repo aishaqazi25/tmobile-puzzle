ebd76d5bf10028555b70640a2fbb3cf325c501f0
Aisha Qazi <aisha_qazi@infosys.com>
2020-01-20 22:11:01 +0530

    Fixes and code improvement

--- libs/shared/ui/chart/src/lib/chart/chart.component.spec.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartComponent } from './chart.component';
+import { SharedUiChartModule } from './../shared-ui-chart.module';
+import { of } from 'rxjs/internal/observable/of';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,18 +11,31 @@ describe('ChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
+      declarations: [],
+      imports: [
+        SharedUiChartModule,
+        GoogleChartsModule.forRoot()]
     })
-    .compileComponents();
+      .compileComponents();
   }));
-
+  const dataStub$ = {
+    get() {
+      return of([{ date: '2015-01-20', close: '110.35' },
+      { date: '2015-01-21', close: '112.1' }, { date: '2015-01-22', close: '114.7' }], async);
+    }
+  };
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartComponent);
     component = fixture.componentInstance;
+    component.data$ = dataStub$.get();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('call ngoninit', () => {
+    component.ngOnInit();
+    expect(component.chartData).toBeDefined();
   });
 });
--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
@@ -24,6 +26,7 @@ export class StocksComponent implements OnInit {
     { viewValue: 'Three months', value: '3m' },
     { viewValue: 'One month', value: '1m' }
   ];
+  formSubscription: Subscription;
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
@@ -33,7 +36,11 @@ export class StocksComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.stockPickerForm.valueChanges.subscribe(this.fetchQuote);
+    this.formSubscription = this.stockPickerForm.valueChanges.pipe(
+      debounceTime(1000),
+    ).subscribe(() => {
+      this.fetchQuote();
+    });
   }
 
   fetchQuote() {
@@ -41,5 +48,9 @@ export class StocksComponent implements OnInit {
       const { symbol, period } = this.stockPickerForm.value;
       this.priceQuery.fetchQuote(symbol, period);
     }
+  }
+
+  ngOnDestroy() {
+    this.formSubscription.unsubscribe();
   }
 }
