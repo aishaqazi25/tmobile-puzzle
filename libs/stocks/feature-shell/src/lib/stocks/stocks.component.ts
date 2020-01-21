987fa15a33c7529ef1dd2e0c422f3c090e9533c7
Aisha Qazi <aisha_qazi@infosys.com>
2020-01-21 13:27:04 +0530

    Changes for custom date pickers

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,43 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subscription } from 'rxjs';
+
+const API_MAX = 'max';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
 
   quotes$ = this.priceQuery.priceQueries$;
 
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
+  today: Date = new Date();
+  maxDate: Date = new Date();
+
+  formSubscription: Subscription;
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      startDate: [null, Validators.required],
+      endDate: [null, Validators.required]
     });
   }
 
-  ngOnInit() {}
-
-  fetchQuote() {
-    if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
-      this.priceQuery.fetchQuote(symbol, period);
-    }
+  ngOnInit() {
+    this.formSubscription = this.stockPickerForm.valueChanges.subscribe(form => {
+      const { symbol, startDate, endDate } = this.stockPickerForm.value;
+      if (endDate && endDate < this.today) {
+        this.maxDate = endDate;
+      }
+      if (this.stockPickerForm.valid) {
+        this.fetchQuote(symbol, startDate, endDate);
+      }
+    });
+  }
+  fetchQuote(symbol, startDate, endDate) {
+    this.priceQuery.fetchQuote(symbol, API_MAX, startDate, endDate);
+  }
+  ngOnDestroy() {
+    this.formSubscription.unsubscribe();
   }
 }
